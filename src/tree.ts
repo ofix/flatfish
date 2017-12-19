2f2561142740607c91b4b0f80d4427c33814a947
codelighter <shb8845369@gmail.com>
2017-12-19 09:42:22 +0800

    update tree.ts

--- src/tree.ts
+++ src/tree.ts
@@ -2,8 +2,55 @@
  * Created by Administrator on 2017/12/15 0015.
  */
 class CTree{
-    data:string[];
-    constructor(data:string[]){
-        this.data = data;
+    old_tree:any;
+    new_tree:any;
+    constructor(old_tree:any,new_tree:any){
+        this.old_tree = old_tree;
+        this.new_tree = new_tree;
+    }
+    getLocalOldTree(){
+        this.old_tree=[
+            {
+                "id":1,"parent_id":0,"name":"一级菜单",
+            },{
+                "id":2,"parent_id":1,"name":"二级菜单1",
+            },{
+                "id":3,"parent_id":1,"name":"二级菜单2",
+            },{
+                "id":4,"parent_id":2,"name":"三级菜单1",
+            }
+        ];
+        this.new_tree =[
+            {
+                "id":1,"parent_id":0,"name":"一级菜单",
+            },{
+                "id":2,"parent_id":1,"name":"二级菜单1",
+            },{
+                "id":3,"parent_id":1,"name":"二级菜单2",
+            },{
+                "id":4,"parent_id":2,"name":"三级菜单1",
+            },{
+                "id":5,"parent_id":2,"name":"三级菜单2",
+            }
+        ];
+    }
+    static is_callable(func):boolean{
+        return (typeof func === "function");
+    }
+    buildTree(array, callback = null, parent_id = 0, child_node = "children"):any{
+        let tree = [];
+        let that = this;
+        array.forEach((v,k,arr)=>{
+            if (v['parent_id'] == parent_id) {
+                arr.shift(array[k]);
+                let tmp = that.is_callable(callback) ? call_user_func(callback, v) : v;
+                let children = this.buildTree(array, callback, v['id'], child_node);
+                if (children) {
+                    tmp[child_node] = children;
+                }
+                tree.push(tmp);
+            }
+        });
+        return tree;
     }
 }
